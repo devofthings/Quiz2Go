717a148d149b0dbb5752564ba8e56370d45a032c
Praveen Kumar <praveenscience@gmail.com>
2018-10-22 20:41:47 +0100

    Fixed all the syntax errors.
    There were too many. 😞

--- js/demo.js
+++ js/demo.js
@@ -1,249 +1,250 @@
-data = [
-    {
-      "question": "What or who is 'Dracula vampira'?",
-      "answerA": "person",
-      "answerB": "plant",
-      "answerC": "animal",
-      "answerD": "club",
-      "correct": "answerB"
-    },
-    {
-      "question": "How many people live in France (2017)",
-      "answerA": "43 mio",
-      "answerB": "110 mio",
-      "answerC": "67 mio",
-      "answerD": "",
-      "correct": "answerC"
-    },
-    {
-      "question": "What's the capital of Azerbaijan?",
-      "answerA": "Baku",
-      "answerB": "Baka",
-      "answerC": "",
-      "answerD": "",
-      "correct": "answerA"
-    },
-    {
-      "question": "This quiz is...",
-      "answerA": "awesome ;)",
-      "answerB": "",
-      "answerC": "",
-      "answerD": "",
-      "correct": "answerA"
-    },
-    {
-      "question": "Database normalization is the process...",
-      "answerA": " of putting some normal people into a database who can keep things from getting weird.",
-      "answerB": " restructuring a relational database in accordance with a series of so-called normal forms in order to reduce data redundancy and improve data integrity.",
-      "answerC": " of trying to improve the read performance of a database, at the expense of losing some write performance, by adding redundant copies of data or by grouping data.",
-      "answerD": " restructuring a non-relational database in accordance with a series of so-called normal forms in order to reduce data redundancy and improve data integrity.",
-      "correct": " answerB"
-    },
-    {
-      "question": "From which language is the word ‘ketchup’ derived?",
-      "answerA": "French",
-      "answerB": "Malay",
-      "answerC": "English",
-      "answerD": "Hindi",
-      "correct": "answerB"
-    },
-    {
-      "question": "Brie and Camembert are types of which food?",
-      "answerA": "Bread",
-      "answerB": "Wine",
-      "answerC": "Cheese",
-      "answerD": "Meat",
-      "correct": "answerC"
-    },
-    {
-      "question": "In the popular video game series, what type of animal is Sonic?",
-      "answerA": "Dog",
-      "answerB": "Cat",
-      "answerC": "Hedgehog",
-      "answerD": "Mouse",
-      "correct": "answerC"
-    },
-    {
-      "question": "H2O is the chemical compound for...?",
-      "answerA": "Water",
-      "answerB": "Gold",
-      "answerC": "Uranium",
-      "answerD": "Oxygen",
-      "correct": "answerA"
-    },
-    {
-      "question": "What does the ‘MI’ in ‘MI6’ stand for?",
-      "answerA": "Massive Intellect",
-      "answerB": "Mighty Inc.",
-      "answerC": "Military Intelligence",
-      "answerD": "Manchester Inc.",
-      "correct": "answerC"
-    },
-    {
-      "question": "In which religion is Krishna a god?",
-      "answerA": "Judaism",
-      "answerB": "Christianity",
-      "answerC": "Hinduism",
-      "answerD": "Buddhism",
-      "correct": "answerC"
-    },
-    {
-      "question": "In Tolkien's Middle Earth, What Sindarin word was Aragorn known by when he was a child?",
-      "answerA": "Estel - meaning 'hope' or 'trust'",
-      "answerB": "Amdir - meaning 'looking up'",
-      "answerC": "Aeluin - meaning 'blue lake'",
-      "answerD": "Míriel - meaning 'sparkling like jewels'",
-      "correct": "answerA"
-    },
-    {
-      "question": "In Tolkien's Middle Earth, What do the Elves call the Hobbits?",
-      "answerA": "The Mellon",
-      "answerB": "The Periannath",
-      "answerC": "The Onodrim",
-      "answerD": "The Urulóki",
-      "correct": "answerB"
-    },
-    {
-      "question": "What is the correct conversion from inches to meters?".
-      "answerA": "1 inch = 0.003 meters",
-      "answerB": "1 inch = 0.03 meters",
-      "answerC": "1 inch = 0.3 meters",  
-      "answerD": "1 inch = 3 meters",
-      "correct": "answerB"
-    },
-    {
-      "question": "What does the abbreviation OLED stand for?".
-      "answerA": "Odd-Voltage Light Emiting Diode",
-      "answerB": "Ordered Light Emiting Diode",
-      "answerC": "Osfactoric Light Emiting Diode",  
-      "answerD": "Organic Light Emiting Diode",
-      "correct": "answerD"
-    },
-    {
-      "question": "What was Google's originally called?",
-      "answerA": "Googol",
-      "answerB": "Backrub",
-      "answerC": "Alphabet",
-      "answerD": "Google",
-      "correct": "answerB"
-    },
-    {
-      "question": "Which is the most pirated Software?,
-      "answerA": "Adobe Photoshop",
-      "answerB": "Winzip",
-      "answerC": "MSOffice",
-      "answerD": "Steam",
-      "correct": "answerC"
-    },
-    {
-      "question": "Which airport has IATA code of BHD?",
-      "answerA": "Baghdad International Airport",
-      "answerB": "George Best Belfast City Airport",
-      "answerC": "Berlin Brandenburg Airport",
-      "answerD": "Hancock County-Bar Harbor Airport",
-      "correct": "answerB"
-    },
-    {
-      "question": "Which century was William Shakespeare born?",
-      "answerA": "14th century (1396)",
-      "answerB": "15th century (1465)",
-      "answerC": "16th century (1564)",
-      "answerD": "17th century (1695)",
-      "correct": "answerC"
-    {
-      "question": "What is the percentage of water on Earth?",
-      "answerA": "56",
-      "answerB": "80",
-      "answerC": "71",
-      "answerD": "60",
-      "correct": "answerC"
-    },
-    {
-      "question": "Name of planet after earth ?",
-      "answerA": "Mercurius",
-      "answerB": "Saturn",
-      "answerC": "Mars",
-      "answerD": "Jupiter",
-      "correct": "Venus"
-    },
-    {
-      "question": "Who built the Taj Mahal?",
-      "answerA": "Akbar",
-      "answerB": "1Aurangjeb",
-      "answerC": "Shah Jahan",
-      "answerD": "Jhangir",
-      "correct": "answerC"
-    },
-    {
-      "question": "Who built Fatehpur Sikri?",
-      "answerA": "Akbar",
-      "answerB": "1Aurangjeb",
-      "answerC": "Shah Jahan",
-      "answerD": "Jhangir",
-      "correct": "answerA"
-    },
-    {
-      "question": "Who was the first person to discover the route to India?",
-      "answerA": "Columbus",
-      "answerB": "Marco Polo",
-      "answerC": "Vasco Da gama",
-      "answerD": "Julius Ceaser",
-      "correct": "answerC"
-    },
-    {
-      "question": "Who was the first ruler of the Mauryan Empire?",
-      "answerA": "Bimbisar",
-      "answerB": "Ashoka",
-      "answerC": "Chandragupta Maurya",
-      "answerD": "Gopala",
-      "correct": "answerC"
-    },
-    {
-      "question": "What's the capital of Indonesia?",
-      "answerA": "Bali",
-      "answerB": "Bandung",
-      "answerC": "Surabaya",
-      "answerD": "Jakarta",
-      "correct": "Kuala Lumpur"
-    },
-    {
-      "question": "Whose mother complained about his swearing in the film GoodFellas?",
-      "answerA": "Ray Liotta",
-      "answerB": "Joe Pesci",
-      "answerC": "Robert De Niro",
-      "answerD": "Paul Sorvino",
-      "correct": "answerB"
-    },
-    {
-      "question": "What does the Kelvin scale measure?",
-      "answerA": "Height",
-      "answerB": "Toughness",
-      "answerC": "Temperature",
-      "answerD": "Weight",
-      "correct": "answerC"
-    },
-    {
-      "question": "How many strings does a standard guitar have?",
-      "answerA": "2",
-      "answerB": "12",
-      "answerC": "24",
-      "answerD": "6",
-      "correct": "answerD"
-    },
-    {
-      "question": "How did Alice enter Wonderland?",
-      "answerA": "Large Door",
-      "answerB": "Tiny Door",
-      "answerC": "Rabbithole",
-      "answerD": "Window",
-      "correct": "answerC"
-    },
-    {
-      "question": "Who was the final act at Woodstock?",
-      "answerA": "Jimi Hendrix",
-      "answerB": "Janis Joplin",
-      "answerC": "The Who",
-      "answerD": "Joe Cocker",
-      "correct": "answerA"
-    }
-  ];
+var data = [
+  {
+    "question": "What or who is 'Dracula vampira'?",
+    "answerA": "person",
+    "answerB": "plant",
+    "answerC": "animal",
+    "answerD": "club",
+    "correct": "answerB"
+  },
+  {
+    "question": "How many people live in France (2017)",
+    "answerA": "43 mio",
+    "answerB": "110 mio",
+    "answerC": "67 mio",
+    "answerD": "",
+    "correct": "answerC"
+  },
+  {
+    "question": "What's the capital of Azerbaijan?",
+    "answerA": "Baku",
+    "answerB": "Baka",
+    "answerC": "",
+    "answerD": "",
+    "correct": "answerA"
+  },
+  {
+    "question": "This quiz is...",
+    "answerA": "awesome ;)",
+    "answerB": "",
+    "answerC": "",
+    "answerD": "",
+    "correct": "answerA"
+  },
+  {
+    "question": "Database normalization is the process...",
+    "answerA": " of putting some normal people into a database who can keep things from getting weird.",
+    "answerB": " restructuring a relational database in accordance with a series of so-called normal forms in order to reduce data redundancy and improve data integrity.",
+    "answerC": " of trying to improve the read performance of a database, at the expense of losing some write performance, by adding redundant copies of data or by grouping data.",
+    "answerD": " restructuring a non-relational database in accordance with a series of so-called normal forms in order to reduce data redundancy and improve data integrity.",
+    "correct": " answerB"
+  },
+  {
+    "question": "From which language is the word ‘ketchup’ derived?",
+    "answerA": "French",
+    "answerB": "Malay",
+    "answerC": "English",
+    "answerD": "Hindi",
+    "correct": "answerB"
+  },
+  {
+    "question": "Brie and Camembert are types of which food?",
+    "answerA": "Bread",
+    "answerB": "Wine",
+    "answerC": "Cheese",
+    "answerD": "Meat",
+    "correct": "answerC"
+  },
+  {
+    "question": "In the popular video game series, what type of animal is Sonic?",
+    "answerA": "Dog",
+    "answerB": "Cat",
+    "answerC": "Hedgehog",
+    "answerD": "Mouse",
+    "correct": "answerC"
+  },
+  {
+    "question": "H2O is the chemical compound for...?",
+    "answerA": "Water",
+    "answerB": "Gold",
+    "answerC": "Uranium",
+    "answerD": "Oxygen",
+    "correct": "answerA"
+  },
+  {
+    "question": "What does the ‘MI’ in ‘MI6’ stand for?",
+    "answerA": "Massive Intellect",
+    "answerB": "Mighty Inc.",
+    "answerC": "Military Intelligence",
+    "answerD": "Manchester Inc.",
+    "correct": "answerC"
+  },
+  {
+    "question": "In which religion is Krishna a god?",
+    "answerA": "Judaism",
+    "answerB": "Christianity",
+    "answerC": "Hinduism",
+    "answerD": "Buddhism",
+    "correct": "answerC"
+  },
+  {
+    "question": "In Tolkien's Middle Earth, What Sindarin word was Aragorn known by when he was a child?",
+    "answerA": "Estel - meaning 'hope' or 'trust'",
+    "answerB": "Amdir - meaning 'looking up'",
+    "answerC": "Aeluin - meaning 'blue lake'",
+    "answerD": "Míriel - meaning 'sparkling like jewels'",
+    "correct": "answerA"
+  },
+  {
+    "question": "In Tolkien's Middle Earth, What do the Elves call the Hobbits?",
+    "answerA": "The Mellon",
+    "answerB": "The Periannath",
+    "answerC": "The Onodrim",
+    "answerD": "The Urulóki",
+    "correct": "answerB"
+  },
+  {
+    "question": "What is the correct conversion from inches to meters?",
+    "answerA": "1 inch = 0.003 meters",
+    "answerB": "1 inch = 0.03 meters",
+    "answerC": "1 inch = 0.3 meters",  
+    "answerD": "1 inch = 3 meters",
+    "correct": "answerB"
+  },
+  {
+    "question": "What does the abbreviation OLED stand for?",
+    "answerA": "Odd-Voltage Light Emiting Diode",
+    "answerB": "Ordered Light Emiting Diode",
+    "answerC": "Osfactoric Light Emiting Diode",  
+    "answerD": "Organic Light Emiting Diode",
+    "correct": "answerD"
+  },
+  {
+    "question": "What was Google's originally called?",
+    "answerA": "Googol",
+    "answerB": "Backrub",
+    "answerC": "Alphabet",
+    "answerD": "Google",
+    "correct": "answerB"
+  },
+  {
+    "question": "Which is the most pirated Software?",
+    "answerA": "Adobe Photoshop",
+    "answerB": "Winzip",
+    "answerC": "MSOffice",
+    "answerD": "Steam",
+    "correct": "answerC"
+  },
+  {
+    "question": "Which airport has IATA code of BHD?",
+    "answerA": "Baghdad International Airport",
+    "answerB": "George Best Belfast City Airport",
+    "answerC": "Berlin Brandenburg Airport",
+    "answerD": "Hancock County-Bar Harbor Airport",
+    "correct": "answerB"
+  },
+  {
+    "question": "Which century was William Shakespeare born?",
+    "answerA": "14th century (1396)",
+    "answerB": "15th century (1465)",
+    "answerC": "16th century (1564)",
+    "answerD": "17th century (1695)",
+    "correct": "answerC",
+  },
+  {
+    "question": "What is the percentage of water on Earth?",
+    "answerA": "56",
+    "answerB": "80",
+    "answerC": "71",
+    "answerD": "60",
+    "correct": "answerC"
+  },
+  {
+    "question": "Name of planet after earth ?",
+    "answerA": "Mercurius",
+    "answerB": "Saturn",
+    "answerC": "Mars",
+    "answerD": "Jupiter",
+    "correct": "Venus"
+  },
+  {
+    "question": "Who built the Taj Mahal?",
+    "answerA": "Akbar",
+    "answerB": "1Aurangjeb",
+    "answerC": "Shah Jahan",
+    "answerD": "Jhangir",
+    "correct": "answerC"
+  },
+  {
+    "question": "Who built Fatehpur Sikri?",
+    "answerA": "Akbar",
+    "answerB": "1Aurangjeb",
+    "answerC": "Shah Jahan",
+    "answerD": "Jhangir",
+    "correct": "answerA"
+  },
+  {
+    "question": "Who was the first person to discover the route to India?",
+    "answerA": "Columbus",
+    "answerB": "Marco Polo",
+    "answerC": "Vasco Da gama",
+    "answerD": "Julius Ceaser",
+    "correct": "answerC"
+  },
+  {
+    "question": "Who was the first ruler of the Mauryan Empire?",
+    "answerA": "Bimbisar",
+    "answerB": "Ashoka",
+    "answerC": "Chandragupta Maurya",
+    "answerD": "Gopala",
+    "correct": "answerC"
+  },
+  {
+    "question": "What's the capital of Indonesia?",
+    "answerA": "Bali",
+    "answerB": "Bandung",
+    "answerC": "Surabaya",
+    "answerD": "Jakarta",
+    "correct": "Kuala Lumpur"
+  },
+  {
+    "question": "Whose mother complained about his swearing in the film GoodFellas?",
+    "answerA": "Ray Liotta",
+    "answerB": "Joe Pesci",
+    "answerC": "Robert De Niro",
+    "answerD": "Paul Sorvino",
+    "correct": "answerB"
+  },
+  {
+    "question": "What does the Kelvin scale measure?",
+    "answerA": "Height",
+    "answerB": "Toughness",
+    "answerC": "Temperature",
+    "answerD": "Weight",
+    "correct": "answerC"
+  },
+  {
+    "question": "How many strings does a standard guitar have?",
+    "answerA": "2",
+    "answerB": "12",
+    "answerC": "24",
+    "answerD": "6",
+    "correct": "answerD"
+  },
+  {
+    "question": "How did Alice enter Wonderland?",
+    "answerA": "Large Door",
+    "answerB": "Tiny Door",
+    "answerC": "Rabbithole",
+    "answerD": "Window",
+    "correct": "answerC"
+  },
+  {
+    "question": "Who was the final act at Woodstock?",
+    "answerA": "Jimi Hendrix",
+    "answerB": "Janis Joplin",
+    "answerC": "The Who",
+    "answerD": "Joe Cocker",
+    "correct": "answerA"
+  }
+];
